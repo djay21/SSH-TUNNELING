a1bfe378d9d002ee51f9c03fd1cc94c85b8a4115
Dheeraj kumar Gupta <47410636+djay21@users.noreply.github.com>
2020-09-18 13:17:49 +0530

    Update ngrok_setup.sh

--- ngrok_setup.sh
+++ ngrok_setup.sh
@@ -2,5 +2,6 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable*
 rm -rf ngrok-stable*
 ./ngrok authtoken 1gZzovb2fSLK9w6VMcmqaCKzuV8_4PtYsi8thzCQjgDcDXHd1
+./ngork http 8001
 #./ngrok tcp 22
 #./ngrok http 8001
